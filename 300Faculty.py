a48f6ab2537700c2119c0d7618d593458bb6bd0c
Rohan <rohanaras@gmail.com>
2015-04-16 12:00:40 -0700

    navigate tree

--- 300Faculty.py
+++ 300Faculty.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
+# turn webpage into beautiful soup object
 url = 'http://evergreen.edu/faculty/index'
 response = urllib.request.urlopen(url, data=None)
 html = response.read()
 soup = BeautifulSoup(html)
 
-f = open('page.html', 'w')
+#f = open('page.html', 'w')
 
-f.write(soup.prettify())
+#f.write(soup.prettify())
+
+print(soup.body)
