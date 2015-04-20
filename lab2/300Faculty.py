aed671aaf184626f7d19be80ca51e8a7e53eba03
rohan aras <rohanaras@gmail.com>
2015-04-20 11:48:32 -0700

    am able to get phd info. only ended up with 177 phd'd faculty though

--- lab2/300Faculty.py
+++ lab2/300Faculty.py
@@ -1,17 +1,43 @@
+# Rohan Aras
+
+
 from bs4 import BeautifulSoup
 import urllib.request
 
 # turn webpage into beautiful soup object
-url = 'http://evergreen.edu/faculty/index'
-response = urllib.request.urlopen(url, data=None)
-html = response.read()
-soup = BeautifulSoup(html)
+def url_to_beautifulsoup(url):
+	response = urllib.request.urlopen(url, data=None)
+	html = response.read()
+	soup = BeautifulSoup(html)
+	return soup
 
-f = open('derp.html', 'w')
+# get soup for main page
+main_url = 'http://evergreen.edu/faculty/index' 
+main_soup = url_to_beautifulsoup(main_url)
 
-# f.write(soup.tbody.prettify())
-soup_faculty_table = soup.tbody
-faculty_box = soup_faculty_table.find_all('tr')
+# navigate toward faculty table
+soup_faculty_table = main_soup.tbody
+soup_faculty_box = soup_faculty_table.find_all('tr')
 
-for i in faculty_box:
-	print(i.contents)
+# generic faculty page
+faculty_url = 'http://evergreen.edu/faculty/instructor/'
+
+no_name_count = 0 #debug
+
+# extract bio from faculty page 
+for i in soup_faculty_box:
+	current_faculty_url = faculty_url + i['id']
+	faculty_soup = url_to_beautifulsoup(current_faculty_url)
+	soup_bio = faculty_soup.find('p', {'class' : 'bio'})
+	try:
+		str_bio = soup_bio.contents[0]
+		split_str_bio = str_bio.split('Ph.D., ')
+		print(split_str_bio[1] + '\n')
+	except AttributeError:
+		print(i['id'] + ' does not have a bio.\n')
+		no_name_count += 1
+	except IndexError:
+		print(i['id'] + ' does not have a phd\n')
+		no_name_count += 1
+
+print(no_name_count)
