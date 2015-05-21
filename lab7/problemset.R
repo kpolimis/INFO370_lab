0eb10d542e7a7991689d805ed9bf899e5baf91e6
rohan <rohanaras@gmail.com>
2015-05-21 00:23:45 -0700

    lab7

--- lab7/problemset.R
+++ lab7/problemset.R
@@ -2,4 +2,21 @@
 trip1 = c(20,32,35,34,40,51,52,56,57,68)
 trip2 = c(23,34,36,44,42,51,54,57,54,62)
 
+# a)
 plot(trip1, type = 'l')
+lines(trip2)
+
+# b)
+
+# c) 
+cor(trip1, trip2)
+
+## Question 3 ##
+trip2b = trip2+30
+cor(trip1, trip2b)
+
+trip1b = trip1*100 
+cor(trip1b, trip2)
+
+## Question 4 ##
+
